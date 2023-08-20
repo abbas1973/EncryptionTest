4f92d9e99e4f6f2f76c0bb5ce8733cb37ee105c0
abbas1973 <abbas.mn1973@gmail.com>
2023-08-20 18:11:36 +0330

    Add project files.

---
+++ EncryptionProject/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utilities.Extentions;
+
+namespace EncryptionProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string EncryptionKey;
+        public HomeController(IConfiguration config)
+        {
+            EncryptionKey = config.GetSection("EncryptionKey").Get<string>();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string plainText)
+        {
+            ViewBag.PalinText = plainText;
+            var encryptedText = plainText.EncryptString(EncryptionKey);
+            return View("index",encryptedText);
+        }
+    }
+}
